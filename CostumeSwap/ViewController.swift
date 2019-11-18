529ca52996feb41d941efdfede21d734f7c87c9c
Christopher Thompson <chth2116@gmail.com>
2019-11-18 11:12:44 -0700

    Initial Commit

---
+++ CostumeSwap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CostumeSwap
+//
+//  Created by Christopher Thompson on 11/18/19.
+//  Copyright © 2019 Joe Jappster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
